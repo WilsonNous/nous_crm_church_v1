1272c972045b97a821943b99eb17e8fbd2dddf55
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2025-10-02 15:05:44 -0300

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,1 +1,9 @@
+def register_routes(app):
+    from . import auth, visitantes, dashboard, ia, eventos, webhooks
+    auth.register(app)
+    visitantes.register(app)
+    dashboard.register(app)
+    ia.register(app)
+    eventos.register(app)
+    webhooks.register(app)
 
