39e3c7a0e0ce695f66b4cfbd30d64bdaad5d4171
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2025-10-06 14:22:05 -0300

    Update menu_routes.py

--- menu_routes.py
+++ menu_routes.py
@@ -51,3 +51,7 @@ def campanhas_page():
 @menu_bp.app_errorhandler(404)
 def not_found_error(error):
     return render_template("404.html"), 404
+
+@menu_bp.app_errorhandler(500)
+def internal_error(error):
+    return render_template("500.html"), 500
