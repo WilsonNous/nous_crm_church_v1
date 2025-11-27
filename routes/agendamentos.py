426558654d6b79fed9ef203b2cdf598bd0057a11
WilsonNous <107373913+WilsonNous@users.noreply.github.com>
2025-11-27 12:52:16 -0300

    Create agendamentos.py

---
+++ routes/agendamentos.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, request, jsonify
+from models import db, Space, Reserva
+
+bp_agenda = Blueprint("agenda", __name__)
+
+@bp_agenda.route("/api/espacos/listar")
+def listar_espacos():
+    espacos = Space.query.all()
+    return jsonify({"espacos": [ {"id": e.id, "nome": e.nome} for e in espacos ]})
+
+
+@bp_agenda.route("/api/reservas/listar/<int:space_id>/<data>")
+def listar_reservas(space_id, data):
+    reservas = Reserva.query.filter_by(space_id=space_id, data=data).all()
+    return jsonify({"reservas": [
+        {
+            "hora_inicio": r.hora_inicio.strftime("%H:%M"),
+            "hora_fim": r.hora_fim.strftime("%H:%M"),
+            "finalidade": r.finalidade
+        } for r in reservas
+    ]})
+
+
+@bp_agenda.route("/api/reservas/nova", methods=["POST"])
+def nova_reserva():
+    data = request.json
+
+    reserva = Reserva(
+        space_id=data["space_id"],
+        nome=data["nome"],
+        telefone=data["telefone"],
+        finalidade=data["finalidade"],
+        data=data["data"],
+        hora_inicio=data["hora_inicio"],
+        hora_fim=data["hora_fim"]
+    )
+
+    db.session.add(reserva)
+    db.session.commit()
+
+    return jsonify({"message": "Reserva registrada com sucesso!"})
